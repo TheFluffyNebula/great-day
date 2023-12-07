8193e6d1bbba13c3e1ce3c5d9f63bc5fc8038f1f
Jason Yin <jyin23@ncsu.edu>
2023-12-06 23:20:43 -0500

    wit's end

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import './App.css';
 import Integer from './components/Integer';
 import Button from './components/Button'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // when updating to database, increment() becomes async w/ await from db data
 function App() {
-  const [value, setValue] = useState(0)
-  const increment = () => {
-    console.log("hi")
-    setValue(value + 1)
+  const [integers, setIntegers] = useState([])
+
+  // handles "side-effects"
+  useEffect(() => {
+    const getIntegers = async () => {
+      const integersFromServer = await fetchIntegers()
+      setIntegers(integersFromServer)
+    }
+    getIntegers()
+  }, []) // dependency array
+
+  // Just copying the tutorial code at this point ;-;
+  const fetchIntegers = async () => {
+    const res = await fetch('http://localhost:5000/integers')
+    const data = await res.json()
+    return data
+  }
+
+  const fetchInteger = async(id) => {
+    const res = await fetch(`http://localhost:5000/integers/${id}`)
+    const data = await res.json()
+    return data
+  }
+
+  const updateInteger = async (id) => {
+    // console.log("btn clicked")
+    const integerToUpdate = await fetchInteger(id)
+    const updInteger = { ...integerToUpdate, 
+      value: integerToUpdate.value + 1 }
+
+    const res = await fetch(`http://localhost:5000/integers${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updInteger)
+    })
+
+    const data = await res.json()
+
+    setIntegers(
+      integers.map((Integer) => 
+      Integer.id === id ? { ...Integer, 
+        value: data.value } : Integer
+      )
+    )
   }
   return (
     <div className="container">
@@ -16,10 +58,12 @@ function App() {
         Great Day
       </header>
       {/* alternative to closing <>*/} 
-      <Integer value={value}/> 
+      {integers.map((integer) => (
+      <Integer key={integer.id} value={integer.value} />
+      ))}
       <Button color='green'
       text='I had a great day :)'
-      onClick={increment}
+      onClick={updateInteger}
        />
     </div>
   );
