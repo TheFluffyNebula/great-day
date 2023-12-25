42f7ff6cbb56d46ce11bdc7b81911cc15a6eb786
Jason Yin <jyin23@ncsu.edu>
2023-12-25 01:01:10 -0500

    successfully got and updated a value in the database!

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -9,41 +9,53 @@
 // For more documentation on playgrounds please refer to
 // https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
 
-// Select the database to use.
-use('mongodbVSCodePlaygroundDB');
+// // Select the database to use.
+// use('mongodbVSCodePlaygroundDB');
 
-// Insert a few documents into the sales collection.
-db.getCollection('sales').insertMany([
-  { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
-  { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 20, 'date': new Date('2014-04-04T11:21:39.736Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 10, 'date': new Date('2014-04-04T21:23:13.331Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 5, 'date': new Date('2015-06-04T05:08:13Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 10, 'date': new Date('2015-09-10T08:43:00Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 5, 'date': new Date('2016-02-06T20:20:13Z') },
-]);
+// // Insert a few documents into the sales collection.
+// db.getCollection('sales').insertMany([
+//   { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
+//   { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
+//   { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
+//   { 'item': 'xyz', 'price': 5, 'quantity': 20, 'date': new Date('2014-04-04T11:21:39.736Z') },
+//   { 'item': 'abc', 'price': 10, 'quantity': 10, 'date': new Date('2014-04-04T21:23:13.331Z') },
+//   { 'item': 'def', 'price': 7.5, 'quantity': 5, 'date': new Date('2015-06-04T05:08:13Z') },
+//   { 'item': 'def', 'price': 7.5, 'quantity': 10, 'date': new Date('2015-09-10T08:43:00Z') },
+//   { 'item': 'abc', 'price': 10, 'quantity': 5, 'date': new Date('2016-02-06T20:20:13Z') },
+// ]);
 
-// Run a find command to view items sold on April 4th, 2014.
-const salesOnApril4th = db.getCollection('sales').find({
-  date: { $gte: new Date('2014-04-04'), $lt: new Date('2014-04-05') }
-}).count();
+// // Run a find command to view items sold on April 4th, 2014.
+// const salesOnApril4th = db.getCollection('sales').find({
+//   date: { $gte: new Date('2014-04-04'), $lt: new Date('2014-04-05') }
+// }).count();
 
-// Print a message to the output window.
-console.log(`${salesOnApril4th} sales occurred in 2014.`);
+// // Print a message to the output window.
+// console.log(`${salesOnApril4th} sales occurred in 2014.`);
 
-// Here we run an aggregation and open a cursor to the results.
-// Use '.toArray()' to exhaust the cursor to return the whole result set.
-// You can use '.hasNext()/.next()' to iterate through the cursor page by page.
-db.getCollection('sales').aggregate([
-  // Find all of the sales that occurred in 2014.
-  { $match: { date: { $gte: new Date('2014-01-01'), $lt: new Date('2015-01-01') } } },
-  // Group the total sales for each product.
-  { $group: { _id: '$item', totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } } }
-]);
+// // Here we run an aggregation and open a cursor to the results.
+// // Use '.toArray()' to exhaust the cursor to return the whole result set.
+// // You can use '.hasNext()/.next()' to iterate through the cursor page by page.
+// db.getCollection('sales').aggregate([
+//   // Find all of the sales that occurred in 2014.
+//   { $match: { date: { $gte: new Date('2014-01-01'), $lt: new Date('2015-01-01') } } },
+//   // Group the total sales for each product.
+//   { $group: { _id: '$item', totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } } }
+// ]);
 
-// init the actual database by running the following once
-// use('GreatDay')
+
+use('GreatDay')
+// init the actual database by running the following once (DONE)
 // db.getCollection('Counter').insertOne(
 //   { 'value': 0 }
 // )
+
+// test 1: get the value
+// findOne returns first document by default
+// findOne( {_id: "658914b8207db4fd3fe3100a"}).value if there were more documents
+// to access the specific field of a document, simply "dot [field name]" (see below)
+console.log(db.getCollection('Counter').findOne().value)
+
+// test 2: update the value
+const curValue = db.getCollection('Counter').findOne().value
+// first field is find criteria, second field is update
+db.getCollection('Counter').updateOne({}, {$set:{value: curValue + 1}})
